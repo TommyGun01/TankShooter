54b8010ff8d8b384193e89ac77b4def94a97aae4
Furkan Cirik <furkan.cirik@elev.ga.lbs.se>
2018-10-10 15:55:22 +0200

    Början
    Ändra inget, kommer fixa allt dw

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+
+    public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        MovePlayer();
+	}
+    public void MovePlayer()
+    {
+        //this.transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
+        player.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject projectile;
+    public Transform projectileSpawn;
+    public float nextFire = 1.0f;
+    public float currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        projectileSpawn = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        shoot();
+	}
+    public void shoot()
+    {
+        currentTime += Time.deltaTime;
+        if(Input.GetButton("Fire1") && currentTime > nextFire)
+        {
+            nextFire += currentTime;
+
+            Instantiate(projectile, projectileSpawn.position, Quaternion.identity);
+
+            nextFire -= currentTime;
+            currentTime = 0.0f;
+        }
+    }
+}
