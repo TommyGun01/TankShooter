7f6514bb9a76397d6400051276649a364cd69954
Furkan Cirik <furkan.cirik@elev.ga.lbs.se>
2018-10-17 13:21:56 +0200

    Alpha

---
+++ Assets/Scripts/MoveProjectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour {
+
+    public Rigidbody2D projectile;
+
+    public float moveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        projectile = this.gameObject.GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        projectile.velocity = new Vector2(0, 1) * moveSpeed;	
+	}
+}
