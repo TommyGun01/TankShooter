ee331c9a3fc8a211f348db4bb11af1a26be086c1
William Thompson <william.thompson@elev.ga.lbs.se>
2018-11-08 12:36:22 +0100

    la till roterande fiende script

---
+++ Assets/Scripts/RotatingObjects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObjects : MonoBehaviour {
+
+    public float tumble;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
