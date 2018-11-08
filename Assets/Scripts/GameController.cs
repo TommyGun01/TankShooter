2e9476bf091aec8645cda114f005cebdaca23537
William Thompson <william.thompson@elev.ga.lbs.se>
2018-11-08 17:29:37 +0100

    Points system

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    //Points
+    public TMPro.TextMeshProUGUI pointsText;
+    public int points = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = points.ToString();
+    }
+}
